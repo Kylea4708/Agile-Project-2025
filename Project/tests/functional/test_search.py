5a2d145403b8d858e6f102c46552f3334b4c6559
Kylea4708 <karriola@my.bcit.ca>
2025-05-07 16:54:52 -0700

    First functional test implemented

---
+++ Project/tests/functional/test_search.py
@@ -0,0 +1,22 @@
+import pytest
+from app import app, db
+from models import Book, User
+
+@pytest.fixture
+def test_client():
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    with app.app_context():
+        db.create_all()
+        db.drop_all()
+    
+def test_booksearch_correct_title(test_client):
+    with app.app_context():
+        db.session.add(Book(title="Flask for Beginners", author="Ben", quantity=10, physical=True))
+        db.session.add(Book(title="Video Game Guide", author="Drew", quantity=20, physical=False))
+        db.session.commit()
+
+    response = test_client.get("/books?q=flask")
+    assert response.status_code == 200
+    assert b"Flask for Beginners" in response.data
+    assert b"Video Game Guide" not in response.data
