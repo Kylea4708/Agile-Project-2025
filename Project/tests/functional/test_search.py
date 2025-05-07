ac425f5dc38f123854b74422a2e25c30821a4f1a
Kylea4708 <karriola@my.bcit.ca>
2025-05-07 16:56:26 -0700

    one test wasn't as inteded

--- Project/tests/functional/test_search.py
+++ Project/tests/functional/test_search.py
@@ -13,7 +13,6 @@ def test_client():
 def test_booksearch_correct_title(test_client):
     with app.app_context():
         db.session.add(Book(title="Flask for Beginners", author="Ben", quantity=10, physical=True))
-        db.session.add(Book(title="Video Game Guide", author="Drew", quantity=20, physical=False))
         db.session.commit()
 
     response = test_client.get("/books?q=flask")
